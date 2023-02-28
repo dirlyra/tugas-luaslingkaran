b69689f3e631d5a9d43619bd657e4eab48d8e415
dirlyra <118912253+dirlyra@users.noreply.github.com>
2023-02-28 12:00:25 +0700

    Add files via upload

---
+++ function_luaslingkaran dirly x rpl (1).js
@@ -0,0 +1,12 @@
+var jariA;
+var jariB;
+var total;
+
+function jumlahLuasLingkaran(a, b){
+    jariA = a
+    jariB = b
+    total = 3.14 * jariA * jariB
+    return total;
+}
+
+alert(jumlahLuasLingkaran(8, 8));
